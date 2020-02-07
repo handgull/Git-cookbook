2edd2f3411bef028b1aa62b28759250a6e0fe6b0
handgull <gullandrea693@gmail.com>
2020-02-07 11:12:02 +0100

    copy code plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,7 +25,8 @@ module.exports = {
       serviceWorker: true,
       popupComponent: 'MySWUpdatePopup',
       updatePopup: true
-    }]
+    }],
+    ['vuepress-plugin-code-copy', true]
   ]
 }
 
